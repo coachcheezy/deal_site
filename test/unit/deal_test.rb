15652f745077db63d2c5ff2669ec0be6b50285f7
Eugene Correia <Eugene.Correia@activenetwork.com>
2012-12-11 01:34:26 -0800

    Story 3

--- test/unit/deal_test.rb
+++ test/unit/deal_test.rb
@@ -7,9 +7,9 @@ class DealTest < ActiveSupport::TestCase
 
   # I think this is a bad test and it fails sometimes
   test "over should honor current time" do
-  	deal = FactoryGirl.create(:deal, :end_at => Time.zone.now + 0.01)
+  	deal = FactoryGirl.create(:deal, :end_at => Time.zone.now + 5.minutes)
   	assert !deal.over?, "Deal should not be over"
-  	sleep 1
+  	deal.end_at = Time.zone.now - 5.minutes
   	assert deal.over?, "Deal should be over"
   end
 end
